a80664068d3213e098fb9e4766195030d60ef3f8
Salman-sajib <sfsajib@gmail.com>
2024-09-07 11:53:52 +0600

    render and style the component and added the search functionality with the vuejs watch function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,50 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from "vue";
+import q from "@/data/quizes.json";
+
+const quizes = ref(q);
+const search = ref("");
+
+watch(search, () => {
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
+</script>
 
 <template>
-  <div>
-    <h1 class="text-4xl">Quiz Game</h1>
+  <div class="app bg-slate-100 bg-main-bg-gradient min-h-screen px-2">
+    <header
+      class="container mx-auto flex gap-5 flex-col items-center justify-center py-5"
+    >
+      <h1 class="text-4xl font-semibold text-slate-900">QuizWiz</h1>
+      <input
+        v-model.trim="search"
+        type="text"
+        placeholder="search..."
+        class="w-full max-w-[420px] text-lg text-slate-900 bg-slate-900/10 border-2 border-none rounded-full px-5 py-2 placeholder:text-slate-500"
+      />
+    </header>
+
+    <!-- cards -->
+    <main
+      class="container mx-auto flex gap-5 flex-wrap items-center justify-center my-5 cursor-pointer"
+    >
+      <div v-if="quizes.length === 0">
+        <h2 class="text-xl text-red-500">Sorry, No match found!</h2>
+      </div>
+      <div
+        v-else
+        v-for="quiz in quizes"
+        :key="quiz.id"
+        class="w-full max-w-[320px] shadow-md flex flex-col gap-2 overflow-hidden rounded-md"
+      >
+        <img class="w-full h-[190px] bg-cover" :src="quiz.img" alt="" />
+        <div class="p-2">
+          <h2 class="text-xl font-semibold">{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }}</p>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
-
-<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "main-bg-gradient":
+          "radial-gradient(circle at 20% 35%, rgba(203, 213, 225, 1), transparent 50%), radial-gradient(circle at 90% 70%, rgba(220, 252, 231, 0.6), transparent 50%), radial-gradient(circle at 50% 50%, rgba(248, 250, 252, 1), transparent 100%)",
+      },
+    },
   },
   plugins: [],
 };
